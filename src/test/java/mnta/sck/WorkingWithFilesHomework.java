0fa4f151534143cda615892fc05aeec9215a45dc
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-03-09 19:01:13 +0300

    Repository created

---
+++ src/test/java/mnta/sck/WorkingWithFilesHomework.java
@@ -0,0 +1,15 @@
+package mnta.sck;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WorkingWithFilesHomework {
+
+    @Test
+    @DisplayName("Чтение и проверка файлов из архива")
+    void readingAndCheckingFilesFromZipArchive() {
+        
+
+
+    }
+}
